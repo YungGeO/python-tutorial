73b887da9b44465b58a275b0d6667448824ae2c1
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 01:36:42 +0200

    if else

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-temperature = 35
+
+temperature = int(input("give me a number"))
 if temperature > 30:
     print(f"it's temperature = {temperature}")
+elif temperature < 20:
+    print("its cold")
+else:
+    print("its fine i guess")
+print("done")
