6cc580881720ec7492a4165d38fbd829d0718081
George Zacharias <zachariasg7@hotmail.gr>
2025-10-26 08:13:30 +0200

    numbers

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
-course = "Strings \"for python\""
-coursetest = "Strings 'for python'"
 
-print(course)
-print(len(course))
-print(course[0])
-print(course[-1])
-print(course[0:3])
-print(course[0:])
-
-first = "George"
-last = "zacharias"
-full_name = f"{first} {last}"
-print(full_name)
-course.upper
-print(course)
-print(len(course))
-print(coursetest)
-print(len(coursetest))
-print(course.find("for"))
-print(course.replace("f", "n"))
-print("for" in course)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
