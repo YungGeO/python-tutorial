3dd042b5cf940d510c62ea75b434b3e1367a3c1a
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 01:47:05 +0200

    logical ops

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-age = int(input("give me age"))
+high_income = True
+good_credit = True
 
-message = f"it's eligible age since he is = {age}" if age >= 18 else f"it's eligible age since he is = {age}"
-print(message)
+if high_income and good_credit:
+    print("Eligible")
