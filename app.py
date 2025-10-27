cbf451a0a006b3482e214ef01f25ab148836387f
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 02:18:31 +0200

    testing

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
-while True:
-    command = input(">")
-    print("ECHO", command)
-    if command.lower() == "quit":
-        break
+x = 0
+for x in range(0, 11, 1):
+    if x % 2 == 0 and x != 0:
+        print(x)
